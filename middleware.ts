0db99ee2704228bda3ff001bb5f88877c4c5a7d0
Aftab3008 <shaikaftab005@mail.com>
2024-05-23 17:48:00 +0530

    Basic UI and auth implementation

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+};
